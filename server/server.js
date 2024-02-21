23750aaa129abfa8246a62f569fe1067c1da41eb
Ronal Garcia <ronalgarcia@Ronals-MBP.attlocal.net>
2024-02-21 16:34:05 -0600

    Linked database to backend

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const axios = require("axios");
 const path = require("path");
+const UserController = require('./UserController/UserController.js')
+
+const mongoose = require('mongoose');
 
 const apiController = require('./controllers/apiController.js')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const mongoURI = 'mongodb+srv://josuerole:Josue5573@ronalgarcia.nskkhq5.mongodb.net/?retryWrites=true&w=majority&appName=RonalGarcia'
+mongoose.connect(mongoURI);                              
 
+const userRouter = express.Router();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/user', userRouter );
+userRouter.post('/', UserController.createUser);
 // Serve static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, "client", "dist")));
 
@@ -27,6 +39,7 @@ app.get("/", (req, res) => {
 
 app.get('/api/search/:cuisine/:distance/:budget/:latitude/:longitude', apiController.formatRequestData, (req, res) => {
   const {restaurantData} = res.locals
+  console.log(restaurantData)
   return res.status(200).json(restaurantData);
 });
 
@@ -49,3 +62,5 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
